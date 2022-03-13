26d9939fe3bf472de4ad43b5b54e83992cad088b
Kyler Jacobson <sermonisabsconditi@protonmail.com>
2022-03-13 10:27:21 -0700

    Over 9000 permissions added

--- spotify-react/server/index.js
+++ spotify-react/server/index.js
@@ -1,77 +1,85 @@
-const express = require('express')
-const request = require('request');
-const dotenv = require('dotenv');
+const express = require("express");
+const request = require("request");
+const dotenv = require("dotenv");
 
-const port = 5000
+const port = 5000;
 
-global.access_token = ''
+global.access_token = "";
 
-dotenv.config()
+dotenv.config();
 
-var spotify_client_id = process.env.SPOTIFY_CLIENT_ID
-var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
+var spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
+var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 
-var spotify_redirect_uri = 'http://localhost:3000/auth/callback'
+var spotify_redirect_uri = "http://localhost:3000/auth/callback";
 
 var generateRandomString = function (length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var text = "";
+    var possible =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
 };
 
 var app = express();
 
-app.get('/auth/login', (req, res) => {
+app.get("/auth/login", (req, res) => {
+    var scope = "streaming user-read-email user-read-private";
 
-  var scope = "streaming user-read-email user-read-private"
-  var state = generateRandomString(16);
+    var scope =
+        "ugc-image-upload user-read-playback-state user-modify-playback-state user-read-private user-follow-modify user-follow-read user-library-modify user-library-read streaming user-read-playback-position playlist-modify-private playlist-read-collaborative app-remote-control user-read-email playlist-read-private user-top-read playlist-modify-public user-read-currently-playing user-read-recently-played";
+    var state = generateRandomString(16);
 
-  var auth_query_parameters = new URLSearchParams({
-    response_type: "code",
-    client_id: spotify_client_id,
-    scope: scope,
-    redirect_uri: spotify_redirect_uri,
-    state: state
-  })
+    var auth_query_parameters = new URLSearchParams({
+        response_type: "code",
+        client_id: spotify_client_id,
+        scope: scope,
+        redirect_uri: spotify_redirect_uri,
+        state: state,
+    });
 
-  res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
-})
+    res.redirect(
+        "https://accounts.spotify.com/authorize/?" +
+            auth_query_parameters.toString()
+    );
+});
 
-app.get('/auth/callback', (req, res) => {
+app.get("/auth/callback", (req, res) => {
+    var code = req.query.code;
 
-  var code = req.query.code;
+    var authOptions = {
+        url: "https://accounts.spotify.com/api/token",
+        form: {
+            code: code,
+            redirect_uri: spotify_redirect_uri,
+            grant_type: "authorization_code",
+        },
+        headers: {
+            Authorization:
+                "Basic " +
+                Buffer.from(
+                    spotify_client_id + ":" + spotify_client_secret
+                ).toString("base64"),
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        json: true,
+    };
 
-  var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    form: {
-      code: code,
-      redirect_uri: spotify_redirect_uri,
-      grant_type: 'authorization_code'
-    },
-    headers: {
-      'Authorization': 'Basic ' + (Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64')),
-      'Content-Type' : 'application/x-www-form-urlencoded'
-    },
-    json: true
-  };
+    request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            access_token = body.access_token;
+            res.redirect("/");
+        }
+    });
+});
 
-  request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      access_token = body.access_token;
-      res.redirect('/')
-    }
-  });
-
-})
-
-app.get('/auth/token', (req, res) => {
-  res.json({ access_token: access_token})
-})
+app.get("/auth/token", (req, res) => {
+    res.json({ access_token: access_token });
+});
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-})
+    console.log(`Listening at http://localhost:${port}`);
+});
