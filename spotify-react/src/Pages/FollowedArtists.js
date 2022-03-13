deba686c0ec0dd2714873596b754c4e5d4abe2fb
Ryan Zimmerman <zimmerry@oregonstate.edu>
2022-03-13 13:25:46 -0700

    Fine-tune followed artists

--- spotify-react/src/Pages/FollowedArtists.js
+++ spotify-react/src/Pages/FollowedArtists.js
@@ -10,30 +10,31 @@ const headers = [
   },
 ];
 
-const listContent = [
-    {
-        text1: 'Kanye West',
-        text2: '100',
-        text3: '5',
-        text4: 'https://thisis-images.scdn.co/37i9dQZF1DZ06evO3nMr04-default.jpg'
-    },
-    {
-        text1: 'Kid Cudi',
-        text2: '10',
-        text3: '2',
-        text4: 'https://thisis-images.scdn.co/37i9dQZF1DZ06evO04TCIU-default.jpg'
-    },
-    {
-        text1: 'Mac Miller',
-        text2: '1',
-        text3: '1',
-        text4: 'https://i.scdn.co/image/ab6761610000e5ebed3b89aa602145fde71a163a'
-    },
-]
+var listContent = []
 
+function FollowedArtists({ token }) {
 
-function FollowedArtists() {
-    return <CustomList title={"Followed Artists"} listContent={listContent} headers={headers} />;
+    const [ artistList, loading ] = useFollowedArtistsSearch(token)
+
+    console.log("loading again")
+
+    if (artistList && artistList.length > 0 && (listContent.length == 0 || listContent[0].text1 !== artistList[0].name)) {
+        artistList.map(artist =>
+            listContent.push({
+                text1: artist.name,
+                text2: artist.followers.total,
+                text3: artist.genres[0],
+                text4: artist.images[2].url
+            },)
+            // && console.log("Pushing", artist.name),
+        )
+    }
+
+    console.log(listContent)
+
+    return ( loading ? <div /> :
+        <CustomList title={"Followed Artists"} listContent={listContent} headers={headers} />
+    )
 }
 
 export default FollowedArtists;
---
+++ spotify-react/src/hooks/followedArtists.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+function useFollowedArtistsSearch(access_token) {
+    const [ artists, setArtists ] = useState([]);
+    const [ loading, setLoading ] = useState(false);
+
+    const controller = new AbortController();
+    useEffect(() => {
+        let ignore = false;
+        async function fetchSearchResults() {
+            let responseBody = {};
+            setLoading(true);
+            const response = await fetch(
+                `https://api.spotify.com/v1/me/following?type=artist`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${access_token}`
+            }});
+            responseBody = await response.json();
+            
+            if(!ignore) {
+                setLoading(false);
+                setArtists(responseBody.artists.items || []);
+            }
+        }
+        fetchSearchResults()
+        return () => {
+            controller.abort();
+            ignore = true;
+        }
+    }, [ access_token ]);
+
+    return [artists, loading];
+}
+
+export default useFollowedArtistsSearch
