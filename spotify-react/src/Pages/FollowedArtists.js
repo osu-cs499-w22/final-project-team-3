56c7cb8d0c38db9aaf8fca16ec27f08aa8e3e92b
Ryan Zimmerman <zimmerry@oregonstate.edu>
2022-03-13 11:51:06 -0700

    Remove comments, change liked albums to main genre

--- spotify-react/src/Pages/FollowedArtists.js
+++ spotify-react/src/Pages/FollowedArtists.js
@@ -6,36 +6,15 @@ const headers = [
   {
     text1: "Artist",
     text2: "Followers",
-    text3: "Liked Albums",
+    text3: "Main Genre",
     text4: "",
   },
 ];
 
-var listContent = [
-    // {
-    //     text1: 'Kanye West',
-    //     text2: '100',
-    //     text3: '5',
-    //     text4: 'https://thisis-images.scdn.co/37i9dQZF1DZ06evO3nMr04-default.jpg'
-    // },
-    // {
-    //     text1: 'Kid Cudi',
-    //     text2: '10',
-    //     text3: '2',
-    //     text4: 'https://thisis-images.scdn.co/37i9dQZF1DZ06evO04TCIU-default.jpg'
-    // },
-    // {
-    //     text1: 'Mac Miller',
-    //     text2: '1',
-    //     text3: '1',
-    //     text4: 'https://i.scdn.co/image/ab6761610000e5ebed3b89aa602145fde71a163a'
-    // },
-]
+var listContent = []
 
 
 function FollowedArtists(props) {
-    // var artistList = []
-    // artistList = getFollowedArtists(props.token)
 
     const [ artistList, loading ] = useFollowedArtistsSearch(props.token)
 
